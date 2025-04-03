b87abad3bd7bbd868ff99c6547eeda29c16f36a9
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-03 14:23:33 +0200

    Update and rename sparsity_varsigma_2D.py to sparsity_2D.py

--- ch3/2D_analysis/sparsity_2D.py
+++ ch3/2D_analysis/sparsity_2D.py
@@ -1,10 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
-import pylops
-from pylops.optimization.sparsity import spgl1 as spgl1_pylops
-import spgl1
 import cvxpy as cp
-
 
 # constructs n^2 x n^2 covariance matrix based on 2D RBF kernel
 def cov_2d(n, L):
@@ -25,37 +21,25 @@ def cov_2d(n, L):
 
     return cov
 
-
-# reconstructs a single s-sparse vector
+# reconstructs a single s-sparse vector with CVX
 def reconstruct(A, n, s, L):
+    
     # ground truth
     xt = np.zeros(n*n)
     pos = np.random.choice(np.arange(n*n), s, replace=False)
     xt[pos] = np.abs(np.random.normal(0, 1, s))
-    # xt[pos] = np.random.normal(0, 1, s)
     
-    # CVX
     y = A @ xt # measurements
     x = cp.Variable(n*n)
     sigma = 0.001 * cp.norm(A @ xt, 2)
     objective = cp.Minimize(cp.norm(x, 1))
-    # constraints = [cp.norm(A @ x - y, 2) <= sigma]
     constraints = [cp.norm(A @ x - y, 2) <= sigma, x>=0]
     problem = cp.Problem(objective, constraints)
     problem.solve()
     error = np.linalg.norm(x.value - xt, ord=2) / np.linalg.norm(xt, ord=2)
 
-    # # measurements
-    # y = Aop * xt
-    # sigma = 0.001 * np.linalg.norm(Aop * xt)
-
-    # # reconstruction and error
-    # xinv = spgl1_pylops(A=Aop, b=y, sigma=sigma, iter_lim=5000)[0]
-    # # xinv = spgl1_pylops(A=Aop, b=y, sigma=0.1, iter_lim=2000)[0]
-    # error = np.linalg.norm(xinv - xt, ord=2) / np.linalg.norm(xt, ord=2)
     return error
     
-
 def compute_results(Aop, n, sparsity_levels, n_runs, L):
     p_success = []
 
@@ -66,19 +50,17 @@ def compute_results(Aop, n, sparsity_levels, n_runs, L):
 
     return p_success
 
-
 np.random.seed(0)
 
 # parameters
-m = 200 #200
-n = 20 #20
-s = 200 #200
+m = 200
+n = 20
+s = 200
 sparsity_levels = range(1, s)
 lengths = [0, 1, np.sqrt(2), 2, np.sqrt(5), 2*np.sqrt(2), 3, np.sqrt(10), np.sqrt(13)]  # 0 represents random Gaussian
-# lengths = [0, np.sqrt(13)]  # 0 represents random Gaussian
 n_runs = 100
 
-# Main loop for processing
+# main loop for processing
 p_success_dict = {}
 baseline_A = np.random.randn(m, n*n)
 for L in lengths:
@@ -89,8 +71,7 @@ for L in lengths:
         cov_stable = cov + np.eye(n*n) * 0.0001
         cov_sqrt = np.linalg.cholesky(cov_stable)
         A = baseline_A @ cov_sqrt.T
-    # Aop = pylops.MatrixMult(A)
     p_success_dict[L] = compute_results(A, n, sparsity_levels, n_runs, L)
 
 # save results
-np.savez('sparsity_results_2D_cvx.npz', lengths=lengths, sparsity_levels=sparsity_levels, p_success_dict=p_success_dict)
+np.savez('sparsity_2D.npz', lengths=lengths, sparsity_levels=sparsity_levels, p_success_dict=p_success_dict)
