ff6d64f021aba6b4e50bf7ef3c1cef669fb2308d
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-03 14:19:38 +0200

    Update and rename ch3/measurements_varsigma_2D.py to ch3/2D_analysis/measurements_2D.py

--- ch3/2D_analysis/measurements_2D.py
+++ ch3/2D_analysis/measurements_2D.py
@@ -1,10 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
-import pylops
-from pylops.optimization.sparsity import spgl1 as spgl1_pylops
-import spgl1
 import cvxpy as cp
-
 
 # constructs n^2 x n^2 covariance matrix based on 2D RBF kernel
 def cov_2d(n, L):
@@ -25,37 +21,24 @@ def cov_2d(n, L):
 
     return cov
 
-
-# reconstructs a single s-sparse vector
+# reconstructs a single s-sparse vector with CVX
 def reconstruct(A, n, s, L):
+    
     # ground truth    
     xt = np.zeros(n*n)
     pos = np.random.choice(np.arange(n*n), s, replace=False)
     xt[pos] = np.abs(np.random.normal(0, 1, s))
-    #xt[pos] = np.random.normal(0, 1, s)
-    
-    # CVX
+
     y = A @ xt # measurements
     x = cp.Variable(n*n)
     sigma = 0.001 * cp.norm(A @ xt, 2)
     objective = cp.Minimize(cp.norm(x, 1))
-    # constraints = [cp.norm(A @ x - y, 2) <= sigma]
     constraints = [cp.norm(A @ x - y, 2) <= sigma, x>=0]
     problem = cp.Problem(objective, constraints)
     problem.solve()
     error = np.linalg.norm(x.value - xt, ord=2) / np.linalg.norm(xt, ord=2)
-
-    # # measurements
-    # y = Aop * xt
-    # sigma = 0.001 * np.linalg.norm(Aop * xt)
     
-
-    # # reconstruction and error
-    # xinv = spgl1_pylops(A=Aop, b=y, sigma=sigma, iter_lim=5000)[0]
-    # # xinv = spgl1_pylops(A=Aop, b=y, sigma=0.1, iter_lim=2000, project=spgl1.norm_l1nn_project)[0]
-    # error = np.linalg.norm(xinv - xt, ord=2) / np.linalg.norm(xt, ord=2)
     return error
-
 
 # computes probabilities of success for different number of measurements
 def compute_results(n_measurements, n, s, L, n_runs):
@@ -73,7 +56,6 @@ def compute_results(n_measurements, n, s, L, n_runs):
             cov_stable = cov + np.eye(n*n) * 0.0001
             cov_sqrt = np.linalg.cholesky(cov_stable)
             A = baseline_A @ cov_sqrt.T
-        #Aop = pylops.MatrixMult(A)
 
         # calculate errors for n_runs
         errors = np.array(Parallel(n_jobs=4)(
@@ -83,19 +65,16 @@ def compute_results(n_measurements, n, s, L, n_runs):
 
     return p_success
 
-
 np.random.seed(0)
 
 # parameters
-m = 400 #400
+m = 400
 n_measurements = range(10, m)  # number of measurements across which to test
-# n_measurements = range(320, m, 200)  # number of measurements across which to test
-n = 20 #20
-s = 40 #40
+n = 20
+s = 40
 lengths = [0, 1, np.sqrt(2), 2, np.sqrt(5), 2*np.sqrt(2), 3, np.sqrt(10), np.sqrt(13)]  # 0 represents random Gaussian
-# lengths = [0, np.sqrt(13)]  # 0 represents random Gaussian
 n_runs = 100
 
 p_success_dict = {L: compute_results(n_measurements, n, s, L, n_runs) for L in lengths}
 
-np.savez('measurements_results_2D_cvx.npz', lengths=lengths, n_measurements=n_measurements, p_success_dict=p_success_dict)
+np.savez('measurements_2D.npz', lengths=lengths, n_measurements=n_measurements, p_success_dict=p_success_dict)
