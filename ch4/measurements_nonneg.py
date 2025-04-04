6b649a2ec6eb2d1b928ddf63b51b46d2d59bb753
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-04 17:04:15 +0200

    Update measurements_nonneg.py

--- ch4/measurements_nonneg.py
+++ ch4/measurements_nonneg.py
@@ -115,3 +115,34 @@ np.savez('measurements_results_nonneg_3apr.npz',
          distributions=distributions, 
          results=results)
 
+### Plot results ###
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    'axes.labelsize': 12,
+    'axes.titlesize': 14,  
+    'xtick.labelsize': 12,  
+    'ytick.labelsize': 12,  
+    'legend.fontsize': 12,  
+    'font.size': 12 
+})
+
+colors = ['black', 'blue', 'orange', 'green', 'red', 'purple']
+
+# Load results
+data = np.load('measurements_results_nonneg_3apr.npz', allow_pickle=True)
+
+n_measurements = data['n_measurements']
+lengths = data['lengths']
+distributions = data['distributions']
+results = data['results'].item()  # Convert to dictionary
+
+# Plot results
+plt.figure()
+for i, dist in enumerate(distributions):
+    plt.plot(n_measurements, results[lengths[0]][dist], label=dist, color=colors[i])
+plt.xlabel('$m$')
+plt.ylabel('Probability of successful reconstruction')
+plt.title(r'$n\times n=20\times 20$, $s=40$, $L=1$')
+plt.legend()
+plt.show()
