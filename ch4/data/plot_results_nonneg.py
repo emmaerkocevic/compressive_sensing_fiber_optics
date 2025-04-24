141fdc0c7014fc3a54dde00616ee10ad31357547
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-24 16:17:09 +0200

    Update plot_results_nonneg.py

--- ch4/data/plot_results_nonneg.py
+++ ch4/data/plot_results_nonneg.py
@@ -1,5 +1,7 @@
-### Plot results ###
+import numpy as np
 import matplotlib.pyplot as plt
+
+'''plots results from this data folder (Fig. 4.4 and Fig. 4.5 in thesis)'''
 
 plt.rcParams.update({
     'axes.labelsize': 12,
@@ -12,15 +14,20 @@ plt.rcParams.update({
 
 colors = ['black', 'blue', 'orange', 'green', 'red', 'purple']
 
-# Load results
-data = np.load('measurements_results_nonneg_3apr.npz', allow_pickle=True)
-
+# load measurement results
+data = np.load('measurements_results_nonneg.npz', allow_pickle=True)
 n_measurements = data['n_measurements']
 lengths = data['lengths']
 distributions = data['distributions']
+results = data['results'].item()  # convert to dictionary
+
+# load sparsity results
+data = np.load('sparsity_results_nonneg_3apr.npz', allow_pickle=True)
+sparsity_levels = data['sparsity_levels']
+distributions = data['distributions']
 results = data['results'].item()  # Convert to dictionary
 
-# Plot results
+# plot measurement results
 plt.figure()
 for i, dist in enumerate(distributions):
     plt.plot(n_measurements, results[lengths[0]][dist], label=dist, color=colors[i])
@@ -30,28 +37,7 @@ plt.title(r'$n\times n=20\times 20$, $s=40$, $L=1$')
 plt.legend()
 plt.show()
 
-
-### Plot results ###
-import matplotlib.pyplot as plt
-
-plt.rcParams.update({
-    'axes.labelsize': 12,
-    'axes.titlesize': 14,  
-    'xtick.labelsize': 12,  
-    'ytick.labelsize': 12,  
-    'legend.fontsize': 12,  
-    'font.size': 12 
-})
-
-colors = ['black', 'blue', 'orange', 'green', 'red', 'purple']
-
-# Load results
-data = np.load('sparsity_results_nonneg_3apr.npz', allow_pickle=True)
-sparsity_levels = data['sparsity_levels']
-distributions = data['distributions']
-results = data['results'].item()  # Convert to dictionary
-
-# Plot results
+plot sparsity results
 plt.figure()
 for i, dist in enumerate(distributions):
     plt.plot(sparsity_levels, results[1][dist], label=dist, color=colors[i])
@@ -60,4 +46,3 @@ plt.ylabel('Probability of successful reconstruction')
 plt.title(r'$m=200$, $n\times n=20\times 20$, $L=1$')
 plt.legend()
 plt.show()
-
