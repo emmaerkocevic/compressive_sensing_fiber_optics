b39a6ec685af97b45fec576c9bfd94566bb6d7b8
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-24 16:23:06 +0200

    Update plot_results_2D.py

--- ch3/2D_analysis/data/plot_results_2D.py
+++ ch3/2D_analysis/data/plot_results_2D.py
@@ -1,6 +1,7 @@
-### Plot results ###
-
+import numpy as np
 import matplotlib.pyplot as plt
+
+'''plots results from this data folder (Fig. 3.13 and Fig. 3.14 in thesis)'''
 
 plt.rcParams.update({
     'axes.labelsize': 12, 
@@ -33,7 +34,6 @@ sqrt_labels = {
     np.sqrt(13): r'\sqrt{13}'
 }
 
-# format labels correctly
 def format_label(L):
     if L in sqrt_labels:
         return rf"$L = {sqrt_labels[L]}$"  
@@ -45,7 +45,7 @@ data = np.load('measurements_results_2D_cvx.npz', allow_pickle=True)
 n_measurements = data['n_measurements']
 p_success_dict = data['p_success_dict'].item()
 
-# plot probability vs. number of measurements
+# plot probabilities across measurement numbers
 plt.figure()
 for key in lengths:
     plt.plot(n_measurements, np.ravel(p_success_dict[key]), label=format_label(key), color=color_map[key])
@@ -53,57 +53,14 @@ plt.legend()
 plt.xlabel('$m$')
 plt.ylabel('Probability of successful reconstruction')
 plt.title(r'$n\times n=20\times20$, $s=40$')
-
 plt.show()
-
-### Plot results ###
-
-import matplotlib.pyplot as plt
-
-plt.rcParams.update({
-    'axes.labelsize': 12, 
-    'axes.titlesize': 14,  
-    'xtick.labelsize': 12,  
-    'ytick.labelsize': 12,  
-    'legend.fontsize': 12,  
-    'font.size': 12  
-})
-
-lengths = [0, 1, np.sqrt(2), 2, np.sqrt(5), 2 * np.sqrt(2), 3, np.sqrt(10), np.sqrt(13)]
-
-color_map = {
-    0: 'blue',            
-    1: 'orange',          
-    np.sqrt(2): 'brown',  
-    2: 'dodgerblue',           
-    np.sqrt(5): 'green',   
-    2 * np.sqrt(2): 'limegreen',  
-    3: 'gold',             
-    np.sqrt(10): 'purple',      
-    np.sqrt(13): 'red' 
-}
-
-sqrt_labels = {
-    np.sqrt(2): r'\sqrt{2}',
-    np.sqrt(5): r'\sqrt{5}',
-    2 * np.sqrt(2): r'2\sqrt{2}',
-    np.sqrt(10): r'\sqrt{10}',
-    np.sqrt(13): r'\sqrt{13}'
-}
-
-# format labels correctly
-def format_label(L):
-    if L in sqrt_labels:
-        return rf"$L = {sqrt_labels[L]}$"  
-    else:
-        return rf"$L = {int(L)}$"
         
 # load sparsity results
 data = np.load('sparsity_results_2D_cvx.npz', allow_pickle=True)
 sparsity_levels = data['sparsity_levels']
 p_success_dict = data['p_success_dict'].item()
 
-# plot probability vs. sparsity Level
+# plot probabilities across sparsity levels
 plt.figure()
 for key in lengths:
     plt.plot(sparsity_levels, np.ravel(p_success_dict[key]), label=format_label(key), color=color_map[key])
@@ -111,5 +68,4 @@ plt.legend()
 plt.xlabel('$s$')
 plt.ylabel('Probability of successful reconstruction')
 plt.title(r'$m=200$, $n\times n=20\times20$')
-
 plt.show()
