bfe155d4aced4ef5811ca0884f7e61b172f94c03
emmaerkocevic <emma.erkocevic@gmail.com>
2025-04-24 16:10:30 +0200

    Update plot_results_nonneg.py

--- ch4/data/plot_results_nonneg.py
+++ ch4/data/plot_results_nonneg.py
@@ -29,3 +29,35 @@ plt.ylabel('Probability of successful reconstruction')
 plt.title(r'$n\times n=20\times 20$, $s=40$, $L=1$')
 plt.legend()
 plt.show()
+
+
+### Plot results ###
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    'axes.labelsize': 12,
+    'axes.titlesize': 14,  
+    'xtick.labelsize': 12,  
+    'ytick.labelsize': 12,  
+    'legend.fontsize': 12,  
+    'font.size': 12 
+})
+
+colors = ['black', 'blue', 'orange', 'green', 'red', 'purple']
+
+# Load results
+data = np.load('sparsity_results_nonneg_3apr.npz', allow_pickle=True)
+sparsity_levels = data['sparsity_levels']
+distributions = data['distributions']
+results = data['results'].item()  # Convert to dictionary
+
+# Plot results
+plt.figure()
+for i, dist in enumerate(distributions):
+    plt.plot(sparsity_levels, results[1][dist], label=dist, color=colors[i])
+plt.xlabel('$s$')
+plt.ylabel('Probability of successful reconstruction')
+plt.title(r'$m=200$, $n\times n=20\times 20$, $L=1$')
+plt.legend()
+plt.show()
+
